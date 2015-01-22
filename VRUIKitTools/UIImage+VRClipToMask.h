93e091a078fc433c39c8a71b8754b339b960908e
Ivan Rublev <ivan@ivanrublev.me>
2015-01-22 11:15:33 +0300

    UIImage+VRClipToMask category is added. It allows to make image by clipping the receiver to color.

---
+++ VRUIKitTools/UIImage+VRClipToMask.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+VRClipToMask.h
+//  VRUIKitTools
+//
+//  Created by Ivan Rublev on 1/22/15.
+//  Copyright (c) 2015 Ivan Rublev http://ivanrublev.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (VRClipToMask)
+
+@end
---
+++ VRUIKitTools/UIImage+VRClipToMask.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+VRClipToMask.m
+//  VRUIKitTools
+//
+//  Created by Ivan Rublev on 1/22/15.
+//  Copyright (c) 2015 Ivan Rublev http://ivanrublev.me. All rights reserved.
+//
+
+#import "UIImage+VRClipToMask.h"
+
+@implementation UIImage (VRClipToMask)
+
+@end
