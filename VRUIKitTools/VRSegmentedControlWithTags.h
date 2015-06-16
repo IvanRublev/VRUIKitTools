39d4ea309c02ee13039c63fd9ccda8135a1e2b98
Ivan Rublev <ivan@ivanrublev.me>
2015-06-16 12:02:33 +0300

    Added support of handling the touch on segment without value change to VRSegmentedControlWithTags. The appropriate delegate is informed on event.

--- VRUIKitTools/VRSegmentedControlWithTags.h
+++ VRUIKitTools/VRSegmentedControlWithTags.h
@@ -8,10 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
+@class VRSegmentedControlWithTags;
+
+@protocol VRSegmentedControlWithTagsSegmentTouchDelegate <NSObject>
+- (void)VRSegmentedControl:(VRSegmentedControlWithTags*)segmentedControl didTouchSegmentWithIndex:(NSUInteger)touchedSegmentIndex;
+@end
+
 @interface VRSegmentedControlWithTags : UISegmentedControl
-- (id)initWithItems:(NSArray *)items tags:(NSArray *)tags;
-- (void)insertSegmentWithImage:(UIImage *)image atIndex:(NSUInteger)segment tag:(NSInteger)tag animated:(BOOL)animated;
-- (void)insertSegmentWithTitle:(NSString *)title atIndex:(NSUInteger)segment tag:(NSInteger)tag animated:(BOOL)animated;
+- (id)initWithItems:(NSArray*)items tags:(NSArray*)tags;
+- (void)insertSegmentWithImage:(UIImage*)image atIndex:(NSUInteger)segment tag:(NSInteger)tag animated:(BOOL)animated;
+- (void)insertSegmentWithTitle:(NSString*)title atIndex:(NSUInteger)segment tag:(NSInteger)tag animated:(BOOL)animated;
 - (void)setTag:(NSUInteger)tag forSegmentAtIndex:(NSUInteger)segment;
 - (NSInteger)tagForSegmentAtIndex:(NSUInteger)segment;
+@property (nonatomic, weak) IBOutlet id<VRSegmentedControlWithTagsSegmentTouchDelegate> segmentTouchDelegate;
 @end
--- VRUIKitTools/VRSegmentedControlWithTags.m
+++ VRUIKitTools/VRSegmentedControlWithTags.m
@@ -18,73 +18,80 @@
 
 #pragma mark -
 #pragma mark Overriding superclass methods
-- (void)makeEmptyTagsOfCount:(NSUInteger)count
-{
+- (void)makeEmptyTagsOfCount:(NSUInteger)count {
     _segmentsTag = [NSMutableArray arrayWithCapacity:count];
     for (int i=0; i<count; i++) {
         [_segmentsTag addObject:@(0)];
     }
 }
 
-- (id)init
-{
+- (id)init {
     if ((self = [super init])) {
         [self makeEmptyTagsOfCount:self.numberOfSegments];
     }
     return self;
 }
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (id)initWithFrame:(CGRect)frame {
     if ((self = [super initWithFrame:frame])) {
         [self makeEmptyTagsOfCount:self.numberOfSegments];
     }
     return self;
 }
 
-- (id)initWithCoder:(NSCoder *)aDecoder
-{
+- (id)initWithCoder:(NSCoder *)aDecoder {
     if ((self = [super initWithCoder:aDecoder])) {
         [self makeEmptyTagsOfCount:self.numberOfSegments];
     }
     return self;
 }
 
-- (id)initWithItems:(NSArray *)items
-{
+- (id)initWithItems:(NSArray*)items {
     if ((self = [super initWithItems:items])) {
         [self makeEmptyTagsOfCount:[items count]];
     }
     return self;
 }
 
-- (void)insertSegmentWithImage:(UIImage *)image atIndex:(NSUInteger)segment animated:(BOOL)animated
-{
+- (void)insertSegmentWithImage:(UIImage *)image atIndex:(NSUInteger)segment animated:(BOOL)animated {
     [self insertSegmentWithImage:image atIndex:segment tag:0 animated:animated];
 }
 
-- (void)insertSegmentWithTitle:(NSString *)title atIndex:(NSUInteger)segment animated:(BOOL)animated
-{
+- (void)insertSegmentWithTitle:(NSString *)title atIndex:(NSUInteger)segment animated:(BOOL)animated {
     [self insertSegmentWithTitle:title atIndex:segment tag:0 animated:animated];
 }
 
-- (void)removeAllSegments
-{
+- (void)removeAllSegments {
     [super removeAllSegments];
     [_segmentsTag removeAllObjects];
 }
 
-- (void)removeSegmentAtIndex:(NSUInteger)segment animated:(BOOL)animated
-{
+- (void)removeSegmentAtIndex:(NSUInteger)segment animated:(BOOL)animated {
     [super removeSegmentAtIndex:segment animated:animated];
     [_segmentsTag removeObjectAtIndex:segment];
 }
 
 
 #pragma mark -
+#pragma mark Override custom touches
+- (void)touchesEnded:(NSSet*)touches withEvent:(UIEvent*)event {
+    NSInteger oldSelectedSegmentIndex = self.selectedSegmentIndex;
+    [super touchesEnded:touches withEvent:event];
+    
+    UITouch* touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self];
+    NSUInteger tapedIndex = (int)(location.x / (self.bounds.size.width / self.numberOfSegments));
+//    NSLog(@"touched segment with index: %d earlier selected segment: %d new selected segment %d", tapedIndex, oldSelectedSegmentIndex, self.selectedSegmentIndex);
+    if (oldSelectedSegmentIndex == self.selectedSegmentIndex) {
+        if ([self.segmentTouchDelegate respondsToSelector:@selector(VRSegmentedControl:didTouchSegmentWithIndex:)]) {
+            [self.segmentTouchDelegate VRSegmentedControl:self didTouchSegmentWithIndex:tapedIndex];
+        }
+    }
+}
+
+#pragma mark -
 #pragma mark Public methods
-- (id)initWithItems:(NSArray *)items tags:(NSArray *)tags
-{
+- (id)initWithItems:(NSArray*)items tags:(NSArray*)tags {
     self = [super initWithItems:items];
     if (self) {
         NSAssert([tags count] == [items count], @"tags array must be the same length as items array.");
@@ -93,25 +100,21 @@
     return self;
 }
 
-- (void)insertSegmentWithImage:(UIImage *)image atIndex:(NSUInteger)segment tag:(NSInteger)tag animated:(BOOL)animated
-{
+- (void)insertSegmentWithImage:(UIImage *)image atIndex:(NSUInteger)segment tag:(NSInteger)tag animated:(BOOL)animated {
     [super insertSegmentWithImage:image atIndex:segment animated:animated];
     [_segmentsTag insertObject:@(tag) atIndex:segment];
 }
 
-- (void)insertSegmentWithTitle:(NSString *)title atIndex:(NSUInteger)segment tag:(NSInteger)tag animated:(BOOL)animated
-{
+- (void)insertSegmentWithTitle:(NSString *)title atIndex:(NSUInteger)segment tag:(NSInteger)tag animated:(BOOL)animated {
     [super insertSegmentWithTitle:title atIndex:segment animated:animated];
     [_segmentsTag insertObject:@(tag) atIndex:segment];
 }
 
-- (void)setTag:(NSUInteger)tag forSegmentAtIndex:(NSUInteger)segment
-{
+- (void)setTag:(NSUInteger)tag forSegmentAtIndex:(NSUInteger)segment {
     [_segmentsTag replaceObjectAtIndex:segment withObject:@(tag)];
 }
 
-- (NSInteger)tagForSegmentAtIndex:(NSUInteger)segment
-{
+- (NSInteger)tagForSegmentAtIndex:(NSUInteger)segment {
     return [[_segmentsTag objectAtIndex:segment] integerValue];
 }
 
